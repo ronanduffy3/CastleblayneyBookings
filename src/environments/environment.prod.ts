a2ca5690cb8cfac5dc15a2a6d368e17fe92df722
Ronan Duffy <48518654+ronanduffy3@users.noreply.github.com>
2023-01-04 21:46:38 +0000

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'castleblayney-bookings',
+    appId: '1:868414805504:web:61acd314edf95148406ae5',
+    storageBucket: 'castleblayney-bookings.appspot.com',
+    apiKey: 'AIzaSyDxstwCKdm3_iMQ0Q14_ibOnO5bz41V_3Y',
+    authDomain: 'castleblayney-bookings.firebaseapp.com',
+    messagingSenderId: '868414805504',
+    measurementId: 'G-13GF0ESZ4B',
+  },
   production: true
 };
