cd7894a283330ad2518cb8bbd804f16f9cf4ecb0
Ronan Duffy <48518654+ronanduffy3@users.noreply.github.com>
2023-01-05 21:12:35 +0000

    added in registration form/login form editied colors of pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,25 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { PropertiesComponent } from './components/properties/properties.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    PropertiesComponent,
+    DashboardComponent,
+    AppHeaderComponent
   ],
+  exports: [ LoginComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule,
